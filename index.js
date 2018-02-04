5bd6136adbf420363233d527eca43ba597eb757a
kazuooooo <matsumotokazuya7@gmail.com>
2018-02-04 22:40:38 +0900

    phase2

--- index.js
+++ index.js
@@ -1,20 +1,43 @@
 "use strict";
 const Alexa = require('alexa-sdk');
 
-exports.handler = function(event, context, callback) {
-  var alexa = Alexa.handler(event, context); // handlerを登録
-  alexa.registerHandlers(handlers);
-  alexa.execute();
+// ステートの定義
+const states = {
+  PLAYING_MODE: '_PLAYING_MODE'
 };
 
-// ここに各intentに対する処理を書いていく
+exports.handler = function(event, context, callback) {
+  var alexa = Alexa.handler(event, context);
+  // alexa.appId = process.env.APP_ID;
+  alexa.registerHandlers(handlers, pokemonHandlers); // 既存のハンドラに加えてpokemonHandlers(下で定義を登録)
+  alexa.execute();
+};
 var handlers = {
-  'LaunchRequest': function () { // 起動時
+  'LaunchRequest': function () {
+    this.handler.state = states.PLAYING_MODE; // stateにPLAYING_MODEをセット
     var message = 'ポケモン言えるかなへようこそ。ポケモンを言ってください。'
     this.emit(':ask', message);
   },
-  'AMAZON.HelpIntent': function () { // 使い方を聞かれたとき
-    var message = 'ポケモンを言っていくゲームです。'
+  'AMAZON.HelpIntent': function () {
+    this.handler.state = states.PLAYING_MODE; // stateにPLAYING_MODEをセット
+    this.handler.state = states.PLAYING_MODE;
+    var message = 'ポケモンを言っていくゲームです。ポケモンを言ってください。'
     this.emit(':ask', message);
-  },
+  }
 };
+
+// PLAYING_MODEの場合はこのhandlerがintentを処理する
+var pokemonHandlers = Alexa.CreateStateHandler(states.PLAYING_MODE, {
+  'PokemonIntent': function() {
+    var pokemon = this.event.request.intent.slots.Pokemon.value;
+    if (pokemons.indexOf(pokemon) > -1) {
+      var message = '正解です!!次のポケモンを言ってください'
+      var reprompt = 'ポケモンを言ってください〜'
+      this.emit(':ask', message, reprompt); // 正解の場合は継続
+    } else {
+      var message = '残念。そんなポケモンはいません。'
+      this.handler.state = '';
+      this.emit(':tell', message); // 不正解の場合は終了
+    }
+  }
+});
